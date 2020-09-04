b7ce7456a4db13afd91deb2d6f2526f7d7e3cc79
HCdev134 <cdev12ucds@gmail.com>
2020-09-03 18:31:01 -0700

    added range input for tts v2.1

--- orig-tts.js
+++ orig-tts.js
@@ -12,8 +12,31 @@ var voiceList = document.querySelector("#voicelist");
 var tts_input = document.querySelector("#tts");
 var tts_button = document.querySelector("#tts-btn");
 
+// Range elements 
+var pitch = document.querySelector('#pitch');
+var pitchValue = document.querySelector('.pitch-value');
+var rate = document.querySelector('#rate');
+var rateValue = document.querySelector('.rate-value');
 
-// TTS API 
+
+// Update Slider Value 
+pitch, rate;
+
+pitchValue.innerHTML = pitch.value; 
+rateValue.innerHTML = rate.value;
+
+pitch.oninput = function () {
+
+
+    console.log(`Pitch Input Adjusted ${this.value}`);
+    pitchValue.innerHTML = this.value;
+}
+rate.oninput = function () {
+    console.log(`Rate Input Adjusted ${this.value}`);
+    rateValue.innerHTML = this.value;
+}
+
+// TTS API , TTS Object Instance 
 var tts_main = window.speechSynthesis;
 
 // Voices Go in this array, which will be used in function getVoices()
@@ -22,19 +45,53 @@ var voices = [];
 
 tts_button.addEventListener("click", () => {
 
-    var toSpeak = new SpeechSynthesisUtterance(tts_input.value);
-    var selectedOption = voiceList.selectedOptions[0].getAttribute("data-name");
-    for(i = 0; i < voices.length ; i++) {
-        if(voices[i].name === selectedOption) {
-          toSpeak.voice = voices[i];
-        }
-      }
-    //   toSpeak.pitch = pitch.value;
-    //   toSpeak.rate = rate.value;
-      tts_main.speak(toSpeak);
-    
-});
+    // var toSpeak = new SpeechSynthesisUtterance(tts_input.value);
+    // var selectedOption = voiceList.selectedOptions[0].getAttribute("data-name");
+    // for (i = 0; i < voices.length; i++) {
+    //     if (voices[i].name === selectedOption) {
+    //         toSpeak.voice = voices[i];
+    //     }
+    // }
+    // //   toSpeak.pitch = pitch.value;
+    // //   toSpeak.rate = rate.value;
+    // if (tts_input.value == null) {
+    //     console.log("condition met input null")
+    //     tts_input.style.border = '2px solid #FF0000';
+    // }
+    // tts_main.speak(toSpeak);
 
+    
+    speak()
+
+});
+function speak(){
+    if (tts_main.speaking) {
+       
+        console.error('speechSynthesis.speaking');
+        return;
+    }
+    if (tts_input.value !== '') {
+    var toSpeak = new SpeechSynthesisUtterance(tts_input.value);
+    toSpeak.onend = function (event) {
+        console.log('SpeechSynthesisUtterance.onend');
+        tts_input.style.opacity = 1; 
+    }
+    toSpeak.onerror = function (event) {
+        console.error('SpeechSynthesisUtterance.onerror');
+    }
+    var selectedOption = voiceList.selectedOptions[0].getAttribute('data-name');
+    for(i = 0; i < voices.length ; i++) {
+      if(voices[i].name === selectedOption) {
+        toSpeak.voice = voices[i];
+        break;
+      }
+    }
+      tts_input.style.opacity = 0.7; 
+    toSpeak.pitch = pitch.value;
+    toSpeak.rate = rate.value;
+    tts_main.speak(toSpeak);
+  }
+}
 
 getVoices();
 
@@ -42,6 +99,7 @@ getVoices();
 if (speechSynthesis == "undefined") {
     speechSynthesis.onvoiceschanged = getVoices;
 }
+
 function getVoices() {
     voices = tts_main.getVoices()
     voiceList.innerHTML = "";
@@ -57,8 +115,17 @@ function getVoices() {
 
     voiceList.selectedOptions = 0;
 }
-
-
+pitch.onchange = function() {
+    pitchValue.textContent = pitch.value;
+  }
+  
+  rate.onchange = function() {
+    rateValue.textContent = rate.value;
+  }
+  
+  voiceList.onchange = function(){
+    speak();
+  }
 // #region 2nd call 
 // document.getElementById("hello-btn").onclick = function speaker() {
 
